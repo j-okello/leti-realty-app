9a9ec87a7e432f93a0ec9298c85151b4d0fa5ed3
James Oduor <70954202+j-okello@users.noreply.github.com>
2025-07-04 15:21:31 +0300

    Update postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,15 +1,9 @@
-import { Ubuntu } from "next/font/google";
+
 
 const config = {
   plugins: {
     "@tailwindcss/postcss": {},
   },
-  theme: {
-    extend: {
-      fontFamily: {
-        Ubuntu: ["Ubuntu", "sans-serif"],
-      },
-    },
-  },
+  
 };
 export default config;
